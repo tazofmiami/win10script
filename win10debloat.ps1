c355f61d97ed34007da570fedb81ec30e9e9caea
tazofmiami <69408013+tazofmiami@users.noreply.github.com>
2020-09-09 10:59:41 -0700

    Update win10debloat.ps1

--- win10debloat.ps1
+++ win10debloat.ps1
@@ -31,13 +31,13 @@ $tweaks = @(
 
 	### External Program Setup
 	"InstallTitusProgs", #REQUIRED FOR OTHER PROGRAM INSTALLS!
-	"InstallAdobe",
-	"Install7Zip",
+	#"InstallAdobe",
+	#"Install7Zip",
 	#"InstallNotepadplusplus",
 	#"InstallMediaPlayerClassic",
 	#"Installgooglechrome",
 	#"reddit-wallpaper-changer",
-	#"zoom",
+	"zoom",
 
 	### Windows Apps
 	"DebloatAll",
